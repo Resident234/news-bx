e53cf79cf8da7df7c8d303d8252bd873dbdc60da
sg <gsu1234@mail.ru>
2023-05-13 18:05:39 +0300

    Таблица для хранения id новостей, для которых в данный момент выполняются задачи

--- local/modules/bx.news/lib/Orm/Tables/NewsProcessingTable.php
+++ local/modules/bx.news/lib/Orm/Tables/NewsProcessingTable.php
@@ -2,22 +2,14 @@
 
 namespace BX\News\Orm\Tables;
 
-use Bitrix\Main\{ArgumentException,
-    ORM\Fields\FloatField,
-    ORM\Fields\Relations\Reference,
-    ORM\Fields\StringField,
-    ORM\Query\Join,
-    SystemException};
+use Bitrix\Main\{ArgumentException, SystemException};
 use Bitrix\Main\ORM\Data\DataManager;
 use Bitrix\Main\ORM\Fields\IntegerField;
-use Bitrix\Main\ORM\Fields\TextField;
-use Sevensuns\Utils\Orm\Collection\GroupResponsibleCollection;
-use Sevensuns\Utils\Orm\Tables\Iblock\GroupsTable;
 
 /**
  * Таблица хранит id новостей, для которых в данный момент выполняются задачи
  *
- * При изменении/добавлении новости запись сюда добавляется. После того, как новость обработана, запись удаляется.
+ * При изменении/добавлении новости запись сюда добавляется. После того как новость обработана, запись удаляется.
  *
  * Class NewsProcessingTable
  * @package BX\News\Orm\Tables
