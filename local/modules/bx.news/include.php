66dc8ea6bf8d606f3573c312b7ae0fd37314f76a
sg <gsu1234@mail.ru>
2023-05-14 10:52:11 +0300

    Библиотека для запросов к сервису

--- local/modules/bx.news/include.php
+++ local/modules/bx.news/include.php
@@ -1,2 +1,7 @@
 <?php
 defined('B_PROLOG_INCLUDED') and (B_PROLOG_INCLUDED === true) or die();
+
+$autoloadPath = $_SERVER['DOCUMENT_ROOT'] . '/local/modules/bx.news/vendor/autoload.php';
+if (file_exists($autoloadPath)) {
+    require_once $autoloadPath;
+}
---
+++ local/modules/bx.news/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '51bb4c849cb585166d46e1c999f034e1505b4a86',
+        'name' => 'bx/news',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bx/news' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '51bb4c849cb585166d46e1c999f034e1505b4a86',
+            'dev_requirement' => false,
+        ),
+        'rmccue/requests' => array(
+            'pretty_version' => 'v2.0.6',
+            'version' => '2.0.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../rmccue/requests',
+            'aliases' => array(),
+            'reference' => '7d0f8394ea7846a7086978318c306123118445dc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
